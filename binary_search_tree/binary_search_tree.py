2a7d87332f0bdc660544bf0a5101da1c06738cec
Nathan Van Wyck <nathanvanwyck@Macintosh-00254bbd8f8a-2.local>
2019-12-18 18:06:08 -0500

    Completed Day 3 Homework

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -12,22 +12,49 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
-
+        if value > self.value: # then it belongs on the right
+            if self.right is None: # so if there is room on the right
+                self.right = BinarySearchTree(value) # extend a branch there
+            else: # but if there isn't room
+                self.right.insert(value) # move right and try again
+        elif value < self.value: # then it belongs on the left
+            if self.left is None: # so if there is room on the left
+                self.left = BinarySearchTree(value) # extend a branch there
+            else: # but if there isn't room
+                self.left.insert(value) # move left and try again
+            
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if target == self.value: # if this is the target we're looking for
+            return True # success!
+        elif target > self.value: # then it belongs on the right
+            if self.right is None: # so if there isn't anything on the right
+                return False # it isn't there
+            else: # but if there is stuff on the right
+                return self.right.contains(target) # check the right
+        elif target < self.value: # then it belongs on the left
+            if self.left is None: # so if there isn't anything on the left
+                return False # it isn't there
+            else: # but if there is stuff on the left
+                return self.left.contains(target) # check the left
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right is None: # if there's nothing to the right
+            return self.value # this is the maximum value
+        else: # but if there is something to the right
+            return self.right.get_max() # it's on the right, so check there
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
-
+        cb(self.value) # call the function cb for the current value
+        if self.right is not None: # and if there's anything to the right
+            self.right.for_each(cb) # apply this for_each function to the right
+        if self.left is not None: # and if there's anything to the left
+            self.left.for_each(cb) # apply this for_each function to the left
+        
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
